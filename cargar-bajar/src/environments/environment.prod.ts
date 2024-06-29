4f97ae808eb4e51f56fdc84fe1260d04819ca56c
Prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-06-28 23:34:07 -0400

    fix

--- cargar-bajar/src/environments/environment.prod.ts
+++ cargar-bajar/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_CARGAR:'34.176.15.24:3000/cargar',
-  API_BAJAR:'34.176.15.24:3000/bajar'
+  API_CARGAR:'https://34.176.15.24:3000/cargar',
+  API_BAJAR:'https://34.176.15.24:3000/bajar'
 };
--- cargar-bajar/src/environments/environment.ts
+++ cargar-bajar/src/environments/environment.ts
@@ -1,10 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
-  API_CARGAR:'34.176.15.24:3000/cargar',
-  API_BAJAR:'34.176.15.24:3000/bajar'
+  production: true,
+  API_CARGAR:'https://34.176.15.24:3000/cargar',
+  API_BAJAR:'https://34.176.15.24:3000/bajar'
 };
-
